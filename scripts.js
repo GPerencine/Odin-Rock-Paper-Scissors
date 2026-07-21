278f7541bfc0a898f2f67ddd309c6b0cbfa0b991
GPerencine <gabrielperencine1@gmail.com>
2026-07-20 23:26:20 -0300

    Implement round logic

--- scripts.js
+++ scripts.js
@@ -18,3 +18,25 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log(`You Tied! ${computerChoice} = ${humanChoice}`)
+    }else if(
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'rock' && computerChoice === 'scissors') || 
+        (humanChoice === 'scissors' && computerChoice === 'paper')                
+    ){
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        humanScore +=1;
+    }else{
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore +=1;
+    }
+    console.log(`Human: ${humanScore} X Computer: ${computerScore}`)
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
